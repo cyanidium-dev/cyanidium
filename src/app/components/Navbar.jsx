1ef6ed57f09b5beb86991fcd417c15aa22db8b73
Nazarii Yankiv <nazar2k10@gmail.com>
2025-04-26 19:42:03 +0300

    Доделан навбар

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,20 +1,38 @@
+"use client";
+
 import { routes } from "@/utils/routes";
 import { useTranslations } from "next-intl";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const t = useTranslations("Navbar");
+  const pathname = usePathname();
+
+  const currentRoute = pathname.split("/")[2] || "/";
+
   return (
-    <ul className="fixed bottom-[65px] left-1/2 -translate-x-1/2 bg-[#e7e6f3] rounded-[4px] shadow-lg p-2 flex gap-[40px] z-[1000]">
-      {routes.map((route) => (
-        <li key={route.path}>
-          <a 
-            href={route.path} 
-            className="text-[#020418] text-[14px] font-semibold uppercase px-4 py-2 rounded-[6px] hover:bg-black/10 transition"
-          >
-            {t(route.name)}
-          </a>
-        </li>
-      ))}
+    <ul className="p-2 h-[55px] fixed bottom-[65px] left-1/2 -translate-x-1/2 bg-[#e7e6f3] rounded-[4px] shadow-lg flex gap-[40px] z-[1000] items-center justify-center">
+      {routes.map((route) => {
+        const routeSlug = route.path === "/" ? "/" : route.path.replace("/", "");
+        const isActive = currentRoute === routeSlug;
+        console.log(currentRoute)
+        console.log(route.path)
+        console.log(isActive)
+        return (
+          <li className={`flex items-center justify-center p-3 rounded-[4px] ${
+            isActive
+              ? "bg-[#020418] text-white"
+              : "text-[#020418] hover:text-[#020418]"
+          }`} key={route.path}>
+            <a 
+              href={route.path} 
+              className="text-[14px] font-raleway font-semibold uppercase whitespace-nowrap"
+            >
+              {t(route.name)}
+            </a>
+          </li>
+        );
+      })}
     </ul>
   );
 }
