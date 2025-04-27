e0a45c395ea9463a20bc474f01fffb5b5116c29e
Nazarii Yankiv <nazar2k10@gmail.com>
2025-04-27 21:04:42 +0300

    фиксы

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -18,9 +18,9 @@ export const footerLinks = [
 // Соц.сети
 
 export const socialLinks = [
-  { name: "Instagram", url: "https://instagram.com", icon: FaInstagram },
+  { name: "Instagram", url: "https://www.instagram.com/cyanidium.dev?igsh=NXpwdHZ5cGR4ZTB6", icon: FaInstagram },
   // { name: "Youtube", url: "https://youtube.com", icon: FaYoutube },
-  { name: "Tiktok", url: "https://tiktok.com", icon: FaTiktok },
-  { name: "Telegram", url: "https://telegram.com", icon: FaTelegram },
+  { name: "Tiktok", url: "https://www.tiktok.com/@cyanidium.dev?_t=ZM-8vt2mvMQAxZ&_r=1", icon: FaTiktok },
+  { name: "Telegram", url: "https://t.me/FreelancerOnMondays", icon: FaTelegram },
   // { name: "Facebook", url: "https://facebook.com", icon: FaFacebook },
 ];
