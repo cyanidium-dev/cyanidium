3cde8be4f290a144f752e8a47fa7bf3759fd361c
Mykhailo <michaelverkhovod@gmail.com>
2025-05-03 13:38:27 +0300

    review change bg img to video player

--- src/app/components/main/Reviews.jsx
+++ src/app/components/main/Reviews.jsx
@@ -4,6 +4,7 @@ import Container from "@/utils/Container";
 import { useRef, useEffect, useState } from "react";
 import {motion} from 'framer-motion'
 import { useTranslations } from "next-intl";
+import VideoPlayer from "./common/VideoPlayer";
 
 export default function Reviews() {
     const scrollContainerRef = useRef(null);
@@ -42,7 +43,7 @@ export default function Reviews() {
         position: t("card_2.position"),
         taskHeader: t("card_2.task_header"),
         task: t("card_2.task"),
-        img: '/image/ava-image-1.jpg'
+        img: '/image/vid1.mp4'
       }, 
       {
         name: t("card_3.name"),
@@ -57,7 +58,7 @@ export default function Reviews() {
         position: t("card_4.position"),
         taskHeader: t("card_4.task_header"),
         task: t("card_4.task"),
-        img: '/image/ava-image-1.jpg'
+        img: '/image/vid2.mp4'
       },
       {
         name: t("card_5.name"),
@@ -72,7 +73,7 @@ export default function Reviews() {
         position: t("card_6.position"),
         taskHeader: t("card_6.task_header"),
         task: t("card_6.task"),
-        img: '/image/ava-image-1.jpg'
+        img: '/image/vid3.mov'
       }, 
     ]
     return (
@@ -106,45 +107,30 @@ export default function Reviews() {
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.2 + index * 0.3}}
               viewport={{ once: true }} key={index} className={`${review.description ? 'min-w-[333px]' : 'min-w-[255px]'} h-[400px] md:h-[435px] lg:min-w-[333px] p-[1px] rounded-lg bg-[radial-gradient(ellipse_at_top_left,_#F0EEFF,_#6D65A3,_#1A1033)]`}>
-                <div className="w-full h-full flex flex-col rounded-[7px]" style={
-                index % 2 === 0
-                  ? {
+                <div className="w-full h-full flex flex-col rounded-[7px]" style={{
                       backgroundImage: "linear-gradient(164deg, #020418 52.91%, #0A157E 204.34%)",
-                    }
-                  : {
-                      backgroundImage: "url(/image/ava-image-1.png)",
-                      backgroundSize: "cover",
-                      backgroundRepeat: "no-repeat",
-                      backgroundPosition: "center",
-                      objectFit: 'cover'
-                    }
-              }>
-                <header className="flex gap-8 items-center p-[28px] md:p-[30px] lg:p-[32px]">
+                    }}>
+                {index % 2 === 0 ? 
+                <>
+                                <header className="flex gap-8 items-center p-[28px] md:p-[30px] lg:p-[32px]">
                   {index % 2 === 0 && <img className="h-[74px] w-[74px] rounded-[50%]" src={review.img} alt="Avatar"/>}
                   <div>
                     <p className="mb-2 text-[20px] font-bold text-[#FFFFFF] uppercase">{review.name}</p>
                     <p className="text-[14px] text-[#FFFFFF]">{review.position}</p>
                   </div>
                 </header>
-                {index % 2 === 0 ? (
-                  <>
-                    <div className="py-[22px] pl-[32px] pr-[32px] lg:pr-[60px] relative bg-[#EAEBFF]">
-                      <h3 className="mb-2 text-[16px] text-black font-semibold uppercase">{review.taskHeader}</h3>
-                      <p className="text-xs md:text-[13px] lg:text-sm text-[#020418]">{review.task}</p>
-                      <img src="/icons/star.png" className="absolute top-3 right-3" alt="Star icon" />
-                    </div>
-                    <div className="px-[32px] py-10 md:py-11 lg:py-[48px] text-xs md:text-[13px] lg:text-[14px] text-[#EAEBFF] font-light break-words whitespace-normal">
-                      {review.description}
-                    </div>
-                  </>
-                ) : (
-                  <div className="mt-auto lg:mb-[10px] p-8 lg:pr-[60px]">
-                    <h3 className="mb-2 text-sm md:text-[15px] lg:text-base text-[#EAEBFF] font-semibold uppercase">{review.taskHeader}</h3>
-                    <p className="text-xs md:text-[13px] lg:text-sm text-[#EAEBFF]">
-                      {review.task}
-                    </p>
+                  <div className="py-[22px] pl-[32px] pr-[32px] lg:pr-[60px] relative bg-[#EAEBFF]">
+                    <h3 className="mb-2 text-[16px] text-black font-semibold uppercase">{review.taskHeader}</h3>
+                    <p className="text-xs md:text-[13px] lg:text-sm text-[#020418]">{review.task}</p>
+                    <img src="/icons/star.png" className="absolute top-3 right-3" alt="Star icon" />
                   </div>
-                )}
+                  <div className="px-[32px] py-10 md:py-11 lg:py-[48px] text-xs md:text-[13px] lg:text-[14px] text-[#EAEBFF] font-light break-words whitespace-normal">
+                      {review.description}
+                  </div>
+                </> : <>
+                  <VideoPlayer videoUrl={review.img}/>
+
+                </>}
                 </div>
               </motion.div>
               )}      
