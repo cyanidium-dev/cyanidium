e75593a962553a4e0f1935b57d03180caf54c22e
Mykhailo <michaelverkhovod@gmail.com>
2025-05-04 19:45:46 +0300

    hero mobile button fix

--- src/app/components/main/hero/HeroMobileButton.jsx
+++ src/app/components/main/hero/HeroMobileButton.jsx
@@ -7,20 +7,10 @@ export default function HeroMobileButton({ onOpen, label }) {
     <div className="flex lg:hidden justify-center pt-7">
       <button
         onClick={onOpen}
-        className="relative w-[310px] h-[48px] rounded-full border border-[#0F41FD] bg-[#EAEBFF] overflow-hidden"
+        className="w-[310px] h-[48px] rounded-full border border-[#0F41FD] bg-[#EAEBFF] overflow-hidden text-[#020418] text-[14px] font-bold uppercase font-raleway"
+        style={{ backgroundImage: "url('/image/buttonbg-1.png')" }}
       >
-        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 h-full w-full z-0" style={{ backgroundImage: "url('/image/buttonbg-1.png')" }}>
-          {/* <Image
-            src="/image/buttonbg-1.png"
-            alt="Hero Background Mobile"
-            fill
-            className="object-cover"
-            priority
-          /> */}
-        </div>
-        <div className="text-[#020418] text-[14px] font-bold uppercase font-raleway">
           {label}
-        </div>
       </button>
     </div>
   );
