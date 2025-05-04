aca3c6417e415346fd55316c576ae7d476863daa
Nazarii Yankiv <nazar2k10@gmail.com>
2025-05-04 18:51:44 +0300

    \n points deleted

--- src/app/components/main/hero/HeroPoints.jsx
+++ src/app/components/main/hero/HeroPoints.jsx
@@ -4,22 +4,16 @@ export default function HeroPoints() {
   const t = useTranslations("HomeSection");
 
   return (
-    <div className="flex flex-col justify-center items-end lg:items-start gap-[40px] z-[10000]">
-      <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
-        {t("point_1").split("\n").map((line, i) => (
-            <span key={`p1-${i}`} className="block">{line}</span>
-        ))}
-        </p>
-        <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
-        {t("point_2").split("\n").map((line, i) => (
-            <span key={`p2-${i}`} className="block">{line}</span>
-        ))}
-        </p>
-        <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
-        {t("point_3").split("\n").map((line, i) => (
-            <span key={`p3-${i}`} className="block">{line}</span>
-        ))}
-        </p>
+    <div className="flex flex-col justify-center items-end lg:items-start gap-[40px] z-[10000] max-w-[155px]">
+    <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
+        {t("point_1")}
+    </p>
+    <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
+        {t("point_2")}
+    </p>
+    <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
+        {t("point_3")}
+    </p>
     </div>
   );
 }
