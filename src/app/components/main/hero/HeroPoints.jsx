92b9aba04feb672078e631d7b1b045acb9d120fb
Nazarii Yankiv <nazar2k10@gmail.com>
2025-05-04 18:56:20 +0300

    z index fix

--- src/app/components/main/hero/HeroPoints.jsx
+++ src/app/components/main/hero/HeroPoints.jsx
@@ -4,16 +4,16 @@ export default function HeroPoints() {
   const t = useTranslations("HomeSection");
 
   return (
-    <div className="flex flex-col justify-center items-end lg:items-start gap-[40px] z-[10000] max-w-[155px]">
-    <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
-        {t("point_1")}
-    </p>
-    <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
-        {t("point_2")}
-    </p>
-    <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
-        {t("point_3")}
-    </p>
+    <div className="flex flex-col justify-center items-end lg:items-start gap-[40px] z-[10] max-w-[155px]">
+        <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
+            {t("point_1")}
+        </p>
+        <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
+            {t("point_2")}
+        </p>
+        <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase bg-gradient-to-b from-[#EAEBFF] to-[#6A8FFF] bg-clip-text text-transparent font-raleway">
+            {t("point_3")}
+        </p>
     </div>
   );
 }
