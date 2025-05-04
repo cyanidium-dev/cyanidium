4240ec51a56872a53062faf5f68db8a3b45da7e3
Nazarii Yankiv <nazar2k10@gmail.com>
2025-05-04 19:37:40 +0300

    fix + points

--- src/app/components/main/hero/HeroPoints.jsx
+++ src/app/components/main/hero/HeroPoints.jsx
@@ -3,17 +3,19 @@ import { useTranslations } from "next-intl";
 export default function HeroPoints() {
   const t = useTranslations("HomeSection");
 
+  const points = [t("point_1"), t("point_2"), t("point_3")];
+
   return (
     <div className="flex flex-col justify-center items-end lg:items-start gap-[40px] z-[10] max-w-[160px]">
-        <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase text-[#6A8FFF] font-raleway">
-            {t("point_1")}
+      {points.map((point, index) => (
+        <p
+          key={index}
+          className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase font-raleway
+                     text-white lg:bg-gradient-to-b lg:from-[#EAEBFF] lg:to-[#6A8FFF] lg:bg-clip-text lg:text-transparent"
+        >
+          {point}
         </p>
-        <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase text-[#6A8FFF] font-raleway">
-            {t("point_2")}
-        </p>
-        <p className="text-right lg:text-left text-[16px] md:text-[24px] font-extralight uppercase text-[#6A8FFF] font-raleway">
-            {t("point_3")}
-        </p>
+      ))}
     </div>
   );
 }
